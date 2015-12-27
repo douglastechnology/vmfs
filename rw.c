1360867fdeaef01aee5efaf8f8748e633f38b662
Jack Douglas <jack.douglas@douglastechnology.co.uk>
2015-12-27 08:23:06 +0000

    block size reduced to 32K

--- rw.c
+++ rw.c
@@ -6,12 +6,12 @@
 
 void write_block(const char *block)
 {
-	char *compressed = malloc(1048576);
-	int compressed_size = LZ4_compress_default(block, compressed, 1048576, 1048576);
+	char *compressed = malloc(32768);
+	int compressed_size = LZ4_compress_default(block, compressed, 32768, 32768);
 
 	unsigned char *key = (unsigned char *)"11111111111111111111111111111111";
 	unsigned char *iv = (unsigned char *)"22222222222222222";
-	unsigned char *encrypted = malloc(1048576);
+	unsigned char *encrypted = malloc(32768);
 	ERR_load_crypto_strings();
 	OpenSSL_add_all_algorithms();
 	OPENSSL_config(NULL);
@@ -19,7 +19,7 @@ void write_block(const char *block)
 
 	char *path = malloc(100);
 	unsigned char *hash = malloc(16);
-	MurmurHash3_x64_128(block, 1048576, 0, hash);
+	MurmurHash3_x64_128(block, 32768, 0, hash);
 	sprintf(path, "/root/%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x", hash[0], hash[1], hash[2], hash[3], hash[4], hash[5], hash[6], hash[7], hash[8], hash[9], hash[10], hash[11], hash[12], hash[13], hash[14], hash[15]);
 
 	FILE *fp = fopen(path, "w");
@@ -36,18 +36,18 @@ void read_block(char *block)
 {
 	unsigned char *key = (unsigned char *)"11111111111111111111111111111111";
 	unsigned char *iv = (unsigned char *)"22222222222222222";
-	unsigned char *encrypted = malloc(1048576);
+	unsigned char *encrypted = malloc(32768);
 	FILE *fp = fopen("/root/de5cd815d1c664b25b173763f66afd1", "r");
-	size_t encrypted_size = fread(encrypted, 1, 1048576, fp);
+	size_t encrypted_size = fread(encrypted, 1, 32768, fp);
 	fclose(fp);
 	
 	ERR_load_crypto_strings();
 	OpenSSL_add_all_algorithms();
 	OPENSSL_config(NULL);
-	char *compressed = malloc(1048576);
+	char *compressed = malloc(32768);
 	int compressed_size = decrypt(encrypted, encrypted_size, key, iv, (unsigned char *)compressed);
 
-	LZ4_decompress_safe(compressed, block, compressed_size, 1048576);
+	LZ4_decompress_safe(compressed, block, compressed_size, 32768);
 
 	free(compressed);
 	free(encrypted);
--- testwrite.c
+++ testwrite.c
@@ -5,13 +5,13 @@
 
 int main(int argc, char *argv[])
 {
-	void *block = malloc(1048576);
-	memset(block, 65, 1048576);
+	void *block = malloc(32768);
+	memset(block, 65, 32768);
 	write_block(block);
-	memset(block, 0, 1048576);
+	memset(block, 0, 32768);
 	read_block(block);
 	FILE *fp = fopen("/root/out", "w");
-	size_t siz = fwrite(block, 1, 1048576, fp);
+	size_t siz = fwrite(block, 1, 32768, fp);
 	fclose(fp);
 	return 0;
 }
